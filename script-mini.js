cb8e5bb3048809c6fc083a8083f79b4987a88c56
eldertubby <leviabe6@gmail.com>
2024-11-22 02:13:35 +0500

    ss

--- script-mini.js
+++ script-mini.js
@@ -1,14 +1,13 @@
 // ==UserScript==
 // @name         Parkour Generator (Mini)
-// @namespace    http://tampermonkey.net/
-// @version      0.7
+// @namespace    https://github.com/elder-tubby/parkour-gen-browser-script/blob/main/script-mini.js
+// @version      0.8
 // @description  Mini version of script to convert elder-tubby's parkour generator data to bonk io maps.
 // @author       eldertubby + Salama
 // @match        https://bonkisback.io/gameframe-release.html
 // @match        https://bonk.io/gameframe-release.html
 // @grant        none
 // @require      https://raw.githubusercontent.com/elder-tubby/parkour-gen-browser-script/refs/heads/main/parkour-gen-script-mini.js
-// @updateURL    https://github.com/elder-tubby/parkour-gen-browser-script/raw/refs/heads/main/tampermonkey-script-mini.js
-// @downloadURL  https://github.com/elder-tubby/parkour-gen-browser-script/raw/refs/heads/main/tampermonkey-script-mini.js
+// @updateURL    https://github.com/elder-tubby/parkour-gen-browser-script/raw/refs/heads/main/script-mini.js
+// @downloadURL  https://github.com/elder-tubby/parkour-gen-browser-script/raw/refs/heads/main/script-mini.js
 // ==/UserScript==
-
