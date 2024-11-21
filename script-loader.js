e8535db933e2ab5c702b0b5903ff2b0ed0823ae9
eldertubby <leviabe6@gmail.com>
2024-11-22 02:30:03 +0500

    aa

---
+++ script-loader.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Parkour Generator
+// @namespace    https://github.com/elder-tubby/parkour-gen-browser-script/blob/main/script-loader.js
+// @version      0.4
+// @description  Converts elder-tubby's parkour generator data to bonk.io maps.
+// @author       eldertubby + Salama
+// @match        https://bonkisback.io/gameframe-release.html
+// @match        https://bonk.io/gameframe-release.html
+// @grant        none
+// @require      https://raw.githubusercontent.com/elder-tubby/parkour-gen-browser-script/refs/heads/main/script.js
+// @require      https://raw.githubusercontent.com/elder-tubby/parkour-gen-browser-script/refs/heads/main/main.js
+// @require      https://raw.githubusercontent.com/elder-tubby/parkour-gen-browser-script/refs/heads/main/ui.js
+// @require      https://raw.githubusercontent.com/elder-tubby/parkour-gen-browser-script/refs/heads/main/css-styles.js
+// @updateURL    https://github.com/elder-tubby/parkour-gen-browser-script/raw/refs/heads/main/script-loader.js
+// @downloadURL  https://github.com/elder-tubby/parkour-gen-browser-script/raw/refs/heads/main/script-loader.js
+// ==/UserScript==
