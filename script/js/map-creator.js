c8bbb4a4a4306b3fcebd00fffe6a76a78fbfe49a
eldertubby <leviabe6@gmail.com>
2024-11-26 15:02:36 +0500

    ss

--- script/js/map-creator.js
+++ script/js/map-creator.js
@@ -43,10 +43,6 @@ function handleMapCreation(currentMapData) {
         const spawnX = inputData.spawn !== null && inputData.spawn !== undefined ? inputData.spawn.spawnX : 99999;
         const spawnY = inputData.spawn !== null && inputData.spawn !== undefined ? inputData.spawn.spawnY : 99999;
 
-        spawnX = inputData.spawn.spawnX;
-        spawnY = inputData.spawn.spawnY;
-
-
         const mapSize = inputData.mapSize !== undefined ? inputData.mapSize : 9;
 
         map.m.a =
